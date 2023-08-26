befccf8e1b12f86cc58d984b3e630b66826644be
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-26 15:23:43 +0300

    урок 5. усл зад №2

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 //Это усложненные задания - №2
+const arr = [
+  "0897867554",
+  "9678654432",
+  "12132446556",
+  "46676788909",
+  "243245567",
+  "6576587809",
+  "23435465758",
+];
+const strArray = function (array) {
+  let newArray = [];
+  let countNewArray = newArray.length;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].substring(0, 1) == "2" || array[i].substring(0, 1) == "4") {
+      newArray[countNewArray] = array[i];
+      countNewArray = newArray.length;
+    }
+  }
+  return newArray;
+};
+
+const primeNumbers = function () {
+  let str = "Простые числа от 1 до 100:";
+  let primeNumArray = [2]; //мой массив простых чисел
+  let count; //кол-во чисел в массиве
+  let k; //счетчик кол-ва делений без остатка
+
+  for (let i = 3; i <= 100; i++) {
+    k = 0;
+    count = primeNumArray.length;
+    for (let t = 0; t < count; t++) {
+      if (i % primeNumArray[t] === 0 && k < 1) {
+        k++;
+      }
+    }
+    if (k === 0) {
+      primeNumArray[count] = i;
+      count++;
+    }
+  }
+  for (let t = 0; t < count; t++) {
+    str =
+      str +
+      "\n" +
+      primeNumArray[t].toString() +
+      " - Делители этого числа: 1 и " +
+      primeNumArray[t].toString();
+  }
+  return str;
+};
+
+console.log(strArray(arr));
+console.log(primeNumbers());
